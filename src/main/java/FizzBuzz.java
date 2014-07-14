63dc1a383cfc5ad93964a2f3ffe795e08b0384f2
somkiat <somkiat.p@gmail.com>
2014-07-14 22:20:03 +0700

    Add example

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+
+	public String showData(Integer number) {
+		if (number % 3 == 0 && number % 5 == 0)
+			return "FizzBuzz";
+		if (number % 3 == 0)
+			return "Fizz";
+		if (number % 5 == 0)
+			return "Buzz";
+		return number.toString();
+	}
+
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,47 @@
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.*;
+import com.mscharhag.oleaster.runner.OleasterRunner;
+
+@RunWith(OleasterRunner.class)
+public class FizzBuzzTest {
+
+	private FizzBuzz fizzBuzz;
+
+	public FizzBuzzTest() {
+		describe("Fizzbuzz", () -> {
+
+			beforeEach(() -> {
+				fizzBuzz = new FizzBuzz();
+			});
+
+			describe("Show data", () -> {
+				it("1 จะต้องแสดงผล 1", () -> {
+					assertEquals("1", fizzBuzz.showData(1));
+				});
+				
+				it("3 จะต้องแสดงผล Fizz", () -> {
+					assertEquals("Fizz", fizzBuzz.showData(3));
+				});
+				
+				it("5 จะต้องแสดงผล Buzz", () -> {
+					assertEquals("Buzz", fizzBuzz.showData(5));
+				});
+				
+				it("ตัวเลขหาร 3 ลงตัว จะต้องแสดงผล Fizz", () -> {
+					assertEquals("Fizz", fizzBuzz.showData(6));
+				});
+				
+				it("ตัวเลขหาร 5 ลงตัว จะต้องแสดงผล Buzz", () -> {
+					assertEquals("Buzz", fizzBuzz.showData(10));
+				});
+				
+				it("ตัวเลขหาร 3 และ 5 ลงตัว จะต้องแสดงผล โรผผBuzz", () -> {
+					assertEquals("FizzBuzz", fizzBuzz.showData(15));
+				});
+			});
+		});
+	}
+
+}
